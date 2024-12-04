ab8f5a8888f6caa893182edf246c1f776933e277
vladgaf <vladxhz@yandex.ru>
2024-12-04 22:43:57 +0300

    clean + locale

--- lib/domain/logger.dart
+++ lib/domain/logger.dart
@@ -1,17 +1,5 @@
-import 'package:flutter/foundation.dart';
-
 class Logger {
-  static Function(dynamic, [Object?, StackTrace?]) get log =>
-      kDebugMode ? _logDebug : _logProd;
-
-  static void _logDebug(dynamic message, [Object? data, StackTrace? stack]) {
-    if (kDebugMode) print(message);
-    if (stack != null) {
-      debugPrintStack(stackTrace: stack, label: data.toString());
-    }
-  }
-
-  static void _logProd(dynamic message, [Object? data, StackTrace? stack]) {
+  static void log(dynamic message, [Object? data, StackTrace? stack]) {
     String m = message.toString();
     if (data != null) m += ':\n$data';
     // ignore: avoid_print
